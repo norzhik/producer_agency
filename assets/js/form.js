e08514cce8a486c565e8821e9bc02d3db2315742
norzhik <34129474+norzhik@users.noreply.github.com>
2017-12-24 21:23:11 +0300

    Update form.js

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,13 +1,16 @@
 var myform = $("form#myform");
 myform.submit(function(event){
 	event.preventDefault();
+var params = myform.serializeArray().reduce(function(obj, item) {
+     obj[item.name] = item.value;
+     return obj;
+  }, {});
 
   // Change to your service ID, or keep using the default service
   var service_id = "default_service";
   var template_id = "audition";
 
   myform.find("button").text("Sending...");
-	console.log("fine");
   emailjs.sendForm(service_id,template_id,"myform")
   	.then(function(){ 
     	alert("Sent!");
